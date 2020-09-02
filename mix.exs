1afe0597477068c0b274e5ce432e00ee2c261e24
Bob Stockdale <bstockdale@zappos.com>
2020-09-02 13:27:07 -0400

    enable os_mon

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule NookBook.MixProject do
   def application do
     [
       mod: {NookBook.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
